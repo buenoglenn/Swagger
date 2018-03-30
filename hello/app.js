1f4ab7fc2d31c2cfb3da47b149e86ad9233b4af7
Glenn Bueno <bueno.glenn@gmail.com>
2018-03-30 16:16:07 +0800

    Change to port 8081
    Change to port 8081

--- hello/app.js
+++ hello/app.js
@@ -4,13 +4,6 @@ var SwaggerExpress = require('swagger-express-mw');
 var app = require('express')();
 module.exports = app; // for testing
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-
-server.listen(server_port, server_ip_address, function () {
-  console.log("Listening on " + server_ip_address + ", port " + server_port)
-});
-
 var config = {
   appRoot: __dirname // required config
 };
@@ -21,17 +14,10 @@ SwaggerExpress.create(config, function (err, swaggerExpress) {
   // install middleware
   swaggerExpress.register(app);
 
-  var port = process.env.PORT || 3000;
+  var port = process.env.PORT || 8081;
   app.listen(port);
 
   if (swaggerExpress.runner.swagger.paths['/hello']) {
     console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
   }
-});
-
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
-
-server.listen(server_port, server_ip_address, function () {
-  console.log("Listening on " + server_ip_address + ", port " + server_port)
 });
